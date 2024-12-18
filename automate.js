cc46ac9d7c83f73c4dedf32c073d9afbcd07d669
Endra Lazuardi <endrafruz@gmail.com>
2024-12-18 09:12:30 +0700

    update with the current surveys

--- automate.js
+++ automate.js
@@ -2,18 +2,20 @@ const automate = () => {
     // using ES5 strict mode, to prevent variable misuse
     'use strict'
 
-    // You can modified these variables, eg: 'Sangat puas' to 'Puas'
-    let multi = 'Sangat puas';
+    // Multiple options for survey responses
+    const multiOptions = ['Sangat puas', 'Sangat setuju', 'Ya'];
     let option = 'Ya';
 
-    // select all options that contains 'Sangat puas' and 'Ya'
-    $('.answerlist1:contains('+multi+'), .answerlist2:contains('+option+')').each(function(){
-        $(this).parent().each(function(){
-            $(this).find('.answerlist2').children().click();
+    // Select all options that contains any of the multiOptions and 'Ya'
+    multiOptions.forEach(multi => {
+        $('.answerlist1:contains('+multi+'), .answerlist2:contains('+option+')').each(function(){
+            $(this).parent().each(function(){
+                $(this).find('.answerlist2').children().click();
+            });
         });
     });
 
-    // If theres a textare, fill it with 'Tidak ada komentar'
+    // If theres a textarea, fill it with 'Tidak ada komentar'
     $(document).ready(function(){
         let val = $.trim($('textarea').val());
         if(val == ''){
@@ -22,7 +24,7 @@ const automate = () => {
     });
 
     // scroll to bottom of page to show submit button
-    if($('.answerlist1:contains('+multi+')').length > 0){
+    if(multiOptions.some(multi => $('.answerlist1:contains('+multi+')').length > 0)){
         window.scrollTo(0,document.body.scrollHeight);
     }
 }
